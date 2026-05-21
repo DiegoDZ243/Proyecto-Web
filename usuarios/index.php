8ffb5c5f9eb94c3c393f74c68c3d6f4a44b32458
Axel Rodriguez Rosas <s23120245@alumnos.itsur.edu.mx>
2026-05-20 20:37:19 -0600

    Add index.php for user CRUD module
    Creacion index usuarios

---
+++ usuarios/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD Usuarios</title>
+</head>
+<body>
+
+    <h1>Modulo Usuarios</h1>
+
+    <a href="agregar.php">
+        <button>Agregar Usuario</button>
+    </a>
+
+    <br><br>
+
+    <a href="mostrar.php">
+        <button>Mostrar Usuarios</button>
+    </a>
+
+</body>
+</html>
