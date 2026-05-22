0f0b48fea16063cc09a3695e27beb97aec800822
denisse20gt <denisse97gt@gmail.com>
2026-05-22 14:33:58 -0600

    new

--- empleados/agregarEmpleados.php
+++ empleados/agregarEmpleados.php
@@ -20,58 +20,49 @@ $resultado = $empleado->getEmpleados();
 
     <title>Empleados Aeropuerto</title>
 
-    <!-- AQUI VA EL CSS -->
     <link rel="stylesheet" href="css/empleados.css">
 
 </head>
 
 <body>
 
-<h1>Agregar Empleado ✈️</h1>
+<h1>Agregar Empleado </h1>
 
-<!-- AQUI VA EL ID DEL FORM -->
 <form id="formEmpleado"
       action="guardar.php"
       method="POST">
 
-    <!-- NOMBRE -->
     <input type="text"
            name="nombre"
            id="nombre"
            placeholder="Nombre">
 
-    <!-- APELLIDO PATERNO -->
     <input type="text"
            name="a_paterno"
            id="a_paterno"
            placeholder="Apellido Paterno">
 
-    <!-- APELLIDO MATERNO -->
     <input type="text"
            name="a_materno"
            id="a_materno"
            placeholder="Apellido Materno">
 
-    <!-- SUELDO -->
     <input type="number"
            step="0.01"
            name="sueldo"
            id="sueldo"
            placeholder="Sueldo">
 
-    <!-- HORA ENTRADA -->
     <label>Hora Entrada</label>
 
     <input type="time"
            name="hora_entrada">
 
-    <!-- HORA SALIDA -->
     <label>Hora Salida</label>
 
     <input type="time"
            name="hora_salida">
 
-    <!-- ID JEFE -->
     <input type="number"
            name="id_jefe"
            placeholder="ID Jefe">
