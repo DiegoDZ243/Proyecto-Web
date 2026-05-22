f52d1b431335dd4fab5201394794039095bbc83f
Axel Rodriguez Rosas <s23120245@alumnos.itsur.edu.mx>
2026-05-22 13:08:44 -0600

    Refactor user edit form for required fields

--- usuarios/editar.php
+++ usuarios/editar.php
@@ -19,40 +19,45 @@ $row = $NewConn->GetRows($result);
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Usuario</title>
     <link rel="stylesheet" href="css/usuarios.css">
 </head>
 <body>
+
 <div class="contenedor">
-<h1>Editar Usuario</h1>
 
-<form action="actualizar.php" method="POST">
+    <h1>Editar Usuario</h1>
 
-    <input type="hidden" name="id_usuario" value="<?php echo $row[0]; ?>">
+    <form action="actualizar.php" method="POST">
 
-    <label>Nombre:</label><br>
-    <input type="text" name="nombre" value="<?php echo $row[1]; ?>"><br><br>
+        <input type="hidden" name="id_usuario" value="<?php echo $row[0]; ?>">
 
-    <label>Apellido Paterno:</label><br>
-    <input type="text" name="a_paterno" value="<?php echo $row[2]; ?>"><br><br>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" value="<?php echo $row[1]; ?>" required>
 
-    <label>Apellido Materno:</label><br>
-    <input type="text" name="a_materno" value="<?php echo $row[3]; ?>"><br><br>
+        <label>Apellido Paterno:</label>
+        <input type="text" name="a_paterno" value="<?php echo $row[2]; ?>" required>
 
-    <label>Fecha Nacimiento:</label><br>
-    <input type="date" name="fecha_nac" value="<?php echo $row[4]; ?>"><br><br>
+        <label>Apellido Materno:</label>
+        <input type="text" name="a_materno" value="<?php echo $row[3]; ?>" required>
 
-    <label>Correo:</label><br>
-    <input type="email" name="correo" value="<?php echo $row[5]; ?>"><br><br>
+        <label>Fecha Nacimiento:</label>
+        <input type="date" name="fecha_nac" value="<?php echo $row[4]; ?>" required>
 
-    <label>Password:</label><br>
-    <input type="text" name="password" value="<?php echo $row[6]; ?>"><br><br>
+        <label>Correo:</label>
+        <input type="email" name="correo" value="<?php echo $row[5]; ?>" required>
 
-    <button type="submit">
-        Actualizar Usuario
-    </button>
+        <label>Password:</label>
+        <input type="text" name="password" value="<?php echo $row[6]; ?>" required>
 
-</form>
+        <button type="submit">
+            Actualizar Usuario
+        </button>
+
+    </form>
+
 </div>
+
 </body>
 </html>
