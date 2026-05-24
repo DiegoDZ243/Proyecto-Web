8a7fe52456fcae3d0c3f97026b3368916a323283
denisse20gt <denisse97gt@gmail.com>
2026-05-24 09:19:05 -0600

    otro

---
+++ empleados/consultarEmpleados.php
@@ -0,0 +1,71 @@
+<?php
+
+include("../Conexion/Conexión.php");
+
+$sql = "SELECT * FROM empleados";
+
+$resultado = mysqli_query($conn, $sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Consultar Empleados</title>
+</head>
+<body>
+
+<h2>Lista de Empleados</h2>
+
+<table border="1">
+
+<tr>
+
+    <th>ID</th>
+    <th>Nombre</th>
+    <th>Apellido Paterno</th>
+    <th>Apellido Materno</th>
+    <th>Sueldo</th>
+    <th>Hora Entrada</th>
+    <th>Hora Salida</th>
+    <th>ID Jefe</th>
+    <th>Acciones</th>
+
+</tr>
+
+<?php while($fila = mysqli_fetch_assoc($resultado)){ ?>
+
+<tr>
+
+    <td><?php echo $fila['id_empleado']; ?></td>
+    <td><?php echo $fila['nombre']; ?></td>
+    <td><?php echo $fila['a_paterno']; ?></td>
+    <td><?php echo $fila['a_materno']; ?></td>
+    <td><?php echo $fila['sueldo']; ?></td>
+    <td><?php echo $fila['hora_entrada']; ?></td>
+    <td><?php echo $fila['hora_salida']; ?></td>
+    <td><?php echo $fila['id_jefe']; ?></td>
+
+    <td>
+
+        <a href="editarEmpleado.php?id=<?php echo $fila['id_empleado']; ?>">
+            Editar
+        </a>
+
+        |
+
+        <a href="eliminarEmpleado.php?id=<?php echo $fila['id_empleado']; ?>">
+            Eliminar
+        </a>
+
+    </td>
+
+</tr>
+
+<?php } ?>
+
+</table>
+
+</body>
+</html>
